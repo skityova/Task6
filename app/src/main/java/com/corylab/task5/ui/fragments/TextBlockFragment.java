07eb0c35377366c28364dab7ebc6206bfa4e6bc8
skityova <anastasiaskityova@yandex.ru>
2023-05-06 00:40:02 +0300

    refactored text block fragment

--- app/src/main/java/com/corylab/task5/ui/fragments/TextBlockFragment.java
+++ app/src/main/java/com/corylab/task5/ui/fragments/TextBlockFragment.java
@@ -1,4 +1,4 @@
-package com.corylab.task5;
+package com.corylab.task5.ui.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.corylab.task5.R;
 
 /**
  * A simple {@link Fragment} subclass.
